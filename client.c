b3629ddbe4e18bb4419b80910fd8bb87cbbe8d48
Bocaletto Luca <58043613+bocaletto-luca@users.noreply.github.com>
2025-06-13 07:39:37 +0200

    Update client.c

--- client.c
+++ client.c
@@ -1,8 +1,9 @@
-// client.c
-// Remote-Cmd Client: si connette, legge da stdin e riceve risposta
+// client.c v2
+// Remote-Cmd Client con help, host/port configurabili e dry-run
 
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,42 +11,78 @@
 
 #define BUF_SZ 4096
 
-int main(int argc, char **argv) {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
-        return 1;
-    }
-    const char *host = argv[1];
-    const char *port = argv[2];
+static void usage(const char *prog) {
+    fprintf(stderr,
+        "Usage: %s -h host -p port [-d]\n"
+        "  -h host    server hostname o IP\n"
+        "  -p port    server TCP port\n"
+        "  -d         dry-run (non invia nulla)\n"
+        "  -?         this help\n",
+        prog);
+    exit(1);
+}
 
-    struct addrinfo hints = { .ai_family = AF_UNSPEC,
-                              .ai_socktype = SOCK_STREAM };
-    struct addrinfo *res;
+int main(int argc, char **argv) {
+    char *host = NULL, *port = NULL;
+    int dry_run = 0, opt;
+
+    while ((opt = getopt(argc, argv, "h:p:d?")) != -1) {
+        switch (opt) {
+        case 'h': host = optarg;         break;
+        case 'p': port = optarg;         break;
+        case 'd': dry_run = 1;           break;
+        case '?':
+        default:  usage(argv[0]);
+        }
+    }
+    if (!host || !port) usage(argv[0]);
+
+    struct addrinfo hints = {
+        .ai_family = AF_UNSPEC,
+        .ai_socktype = SOCK_STREAM
+    }, *res;
     if (getaddrinfo(host, port, &hints, &res) != 0) {
-        perror("getaddrinfo"); return 1;
+        perror("getaddrinfo");
+        return 1;
     }
 
     int sock = socket(res->ai_family, res->ai_socktype, 0);
-    if (sock < 0) { perror("socket"); return 1; }
-    if (connect(sock, res->ai_addr, res->ai_addrlen) < 0) {
-        perror("connect"); return 1;
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
+    if (!dry_run && connect(sock, res->ai_addr, res->ai_addrlen) < 0) {
+        perror("connect");
+        return 1;
     }
     freeaddrinfo(res);
-    printf("Connected to %s:%s. Type commands, ‘exit’ to quit.\n", host, port);
 
+    printf("** %s %s (dry_run=%d) **\n", host, port, dry_run);
     char sendbuf[BUF_SZ], recvbuf[BUF_SZ];
-    while (fgets(sendbuf, BUF_SZ, stdin)) {
-        send(sock, sendbuf, strlen(sendbuf), 0);
-        if (strncmp(sendbuf, "exit", 4) == 0) break;
+    while (fgets(sendbuf, sizeof(sendbuf), stdin)) {
+        // rimuovo eventuale '\n'
+        size_t len = strlen(sendbuf);
+        if (len && sendbuf[len-1]=='\n') sendbuf[len-1]=0;
 
-        // ricevi fino a timeout breve o buffer pieno
+        if (strcmp(sendbuf, "exit")==0) break;
+
+        if (dry_run) {
+            printf("[DRY] WOULD SEND: %s\n", sendbuf);
+            continue;
+        }
+
+        // invio comando
+        send(sock, sendbuf, strlen(sendbuf), 0);
+        send(sock, "\n", 1, 0);
+
+        // ricevo risposta (piccolo timeout implicito)
         ssize_t n;
-        while ((n = recv(sock, recvbuf, BUF_SZ-1, MSG_DONTWAIT)) > 0) {
+        while ((n = recv(sock, recvbuf, sizeof(recvbuf)-1, MSG_DONTWAIT)) > 0) {
             recvbuf[n] = 0;
             fputs(recvbuf, stdout);
         }
     }
 
-    close(sock);
+    if (!dry_run) close(sock);
     return 0;
 }
